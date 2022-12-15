9af8acb6f5e328a58aeb2e9ec8281a8e16803296
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-12-15 19:50:51 +0000

    111

--- js/script.js
+++ js/script.js
@@ -11,13 +11,24 @@
  */
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Template-PWA/sw.js", {
-    scope: "/ICS2O-Template-PWA/",
+  navigator.serviceWorker.register("/ICS2O-Unit6-03-JS/sw.js", {
+    scope: "/ICS2O-Unit6-03-JS/",
   })
 }
 
-// function description
-
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+// main
+const getWeather = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const weatherData = await result.json()
+    document.getElementById("weather-text").innerHTML =
+      "Weather : " + weatherData.weather.main +
+      "<br>Temperature : " + weatherData.main.temp +
+      "<br>Region : " + weatherData.sys.country
+    
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
